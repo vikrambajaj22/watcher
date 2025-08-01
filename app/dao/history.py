252d75b90cc69a43ab1885d6b77683da76f05d64
Vikram Bajaj <vikrambajaj@nyu.edu>
2025-07-31 22:28:44 -0400

    add log

--- app/dao/history.py
+++ app/dao/history.py
@@ -1,10 +1,18 @@
 from app.db import watch_history_collection
+from app.utils.logger import get_logger
 
+logger = get_logger(__name__)
 
 def store_watch_history(data):
     if isinstance(data, list):
         watch_history_collection.delete_many({})
         watch_history_collection.insert_many(data)
+        logger.info("Watch history stored successfully.")
+    else:
+        logger.error("Invalid data format for storing watch history. Expected a list.")
+        raise ValueError("Data must be a list of watch history items.")
 
 def get_watch_history():
-    return list(watch_history_collection.find({}, {"_id": 0}))
+    history = list(watch_history_collection.find({}, {"_id": 0}))
+    logger.info("Watch history retrieved successfully.")
+    return history
